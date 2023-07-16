c6507483f91475b3499ce73203c9b086c28589bc
Othman Nassik <othmannassik1@gmail.com>
2023-07-16 15:51:36 +0100

    Chat package

---
+++ src/main/java/com/example/chatapp/chat/ChatMessage.java
@@ -0,0 +1,18 @@
+package com.example.chatapp.chat;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private MessageType type;
+    private String content;
+    private String sender;
+
+}
