9cab0e98c9525d09b31acd3fc6d99b49a8a402af
Othman Nassik <othmannassik1@gmail.com>
2023-07-16 15:42:53 +0100

    Config package

---
+++ src/main/java/com/example/chatapp/config/WebSocketEventListener.java
@@ -0,0 +1,17 @@
+package com.example.chatapp.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class WebSocketEventListener {
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        // To do
+    }
+}
